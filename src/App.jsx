5338130749e25859a7f6f4903046f344dc8ac7a4
DixitaK <dixitakataria16@gmail.com>
2025-04-02 01:09:06 +0530

    Component with css & timer state logic

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import Stopwatch from './Stopwatch'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import Stopwatch from "./Stopwatch";
 
 function App() {
-  return(<>
-  </>)
+  return (
+    <>
+      <Stopwatch />
+    </>
+  );
 }
 
-export default App
+export default App;
